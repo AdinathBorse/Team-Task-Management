81b06789a0e0c1c6c5441cd7bee649a8c89695e5
Adinath Borse <adinathborse02@gmail.com>
2026-05-19 17:36:49 +0530

    Update axioInstance.js

--- frontend/src/utils/axioInstance.js
+++ frontend/src/utils/axioInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://team-task-management-jch1.onrender.com"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
